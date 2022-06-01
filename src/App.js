f12b94b1b94cb9ca26e10ecbe0c81c255159cb7e
gabrielcannes <gabrielcannes.gc@gmail.com>
2022-06-01 19:19:55 -0300

    Add a delete last number function

--- src/App.js
+++ src/App.js
@@ -37,6 +37,15 @@ function App() {
     setCalc(eval(calc).toString());
   };
 
+  const deleteLast = () => {
+    if (calc == "") {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
   return (
     <div className="App">
       <div className="calculator">
@@ -50,7 +59,7 @@ function App() {
           <button onClick={() => updateCalc("+")}>+</button>
           <button onClick={() => updateCalc("-")}>-</button>
 
-          <button>DEL</button>
+          <button onClick={deleteLast}>DEL</button>
         </div>
 
         <div className="digits">
